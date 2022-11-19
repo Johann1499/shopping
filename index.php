4267d857c79cc80f118b1e325d11aeb32cebbb7b
Johann Christian M. Hernandez <johannhernandez1233@gmail.com>
2022-11-19 15:23:32 +0800

    finished homepage

---
+++ index.php
@@ -0,0 +1,150 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css../styles.css">
+    <title>I Tee | Products</title>
+</head>
+<body>
+    <div class="container">
+        <br>    
+        <h1>I Tee<button type="button" class="btn btn-primary">Cart <span class="badge badge-light">0</span></button></h1>
+        
+        <hr>
+        <br>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt1a.jpg">
+                            <img class="pic-2" src="/img../shirt1b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Black V-neck Tee</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="#">
+                        <img class="pic-1" src="/img../shirt2a.jpg">
+                        <img class="pic-2" src="/img../shirt2b.jpg">
+                    </a>
+                    <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Under Armour Tee</a></h3>
+                        <span class="price">$699.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <img class="pic-1" src="/img../shirt3a.jpg">
+                        <img class="pic-2" src="/img../shirt3b.jpg">
+                        <a href="#">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Orange Pocket Tee</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt4a.jpg">
+                            <img class="pic-2" src="/img../shirt4b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Brown Pocket Tee</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt5a.jpg">
+                            <img class="pic-2" src="/img../shirt5b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt6a.jpg">
+                            <img class="pic-2" src="/img../shirt6b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt7a.jpg">
+                            <img class="pic-2" src="/img../shirt7b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="/img../shirt8a.jpg">
+                            <img class="pic-2" src="/img../shirt8b.jpg">
+                        </a>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
+                        <span class="price">$599.99</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</html>
